47a9229e94854c6aee36eae9c77b82b477b13f89
Jegadeesh2 <sjegadeeshwaran2@gmail.com>
2023-07-24 14:59:04 +0530

    CSS Added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { BsFillInfoCircleFill} from "react-icons/bs"
-import { AiFillHome, AiFillProject} from "react-icons/ai";
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import { AiFillHome, AiFillProject } from "react-icons/ai";
 
 const Navbar = () => {
   return (
     <>
-    <nav>
-    <div className="navbar-container">
-    <Link to="/" className='logo-link'>
-      <h1 className='logo'>Jega..</h1></Link>
-      <div className='links-container'>
-      <Link to="/" className='link'> 
-      <AiFillHome className='icons'/> 
-      <p className='links-title'>Home</p></Link>
-      <Link to="/about" className='link'>
-      <BsFillInfoCircleFill className='icons'/> 
-      <p className='links-title'>About</p></Link>
-      <Link to="/projects" className='link'> 
-      <AiFillProject className='icons'/> 
-      <p className='links-title project-link'>Projects</p></Link>
-      </div>
-    </div>
-    </nav>
+      <nav>
+        <div className="navbar-container">
+          <Link to="/" className="logo-link">
+            <h1 className="logo">Jega..</h1>
+          </Link>
+          <div className="links-container">
+            <NavLink
+              to="/"
+              className="link ({ isActive }) => (isActive ? 'active' : 'inactive')"
+            >
+              <AiFillHome className="icons" />
+              <p className="links-title">Home</p>
+            </NavLink>
+            <NavLink
+              to="/about"
+              className="link ({ isActive }) => (isActive ? 'active' : 'inactive')"
+            >
+              <BsFillInfoCircleFill className="icons" />
+              <p className="links-title">About</p>
+            </NavLink>
+            <NavLink
+              to="/projects"
+              className="link ({ isActive }) => (isActive ? 'active' : 'inactive')"
+            >
+              <AiFillProject className="icons" />
+              <p className="links-title project-link">Projects</p>
+            </NavLink>
+          </div>
+        </div>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
